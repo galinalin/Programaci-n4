6c740f1a4f18201b2b3ca48ba9d7e7a1e97f7167
alumnos alumnos <alumnos@new.edu.ar>
2024-06-10 15:32:10 -0300

    mi primer commit

---
+++ Tarea1.py
@@ -0,0 +1,15 @@
+#ejercicos 1
+a = int(input('cuantos años tenes'))
+if a > 18:
+    print("mayor de edad")
+else:
+    print('menor de edad')
+
+#ejercicos2
+a = str(input('cual es la contraseña?'))
+if a == 'cont1234':
+    print('correcto')
+else:
+    print('contraseña incorrecto')
+
+
---
+++ Tarea2.py
@@ -0,0 +1,4 @@
+#ejercicios4
+a=int(input('Dame un introduzca numero entero positivo'))
+for i in range(a,-1,-1):
+    print(i,end=",")
